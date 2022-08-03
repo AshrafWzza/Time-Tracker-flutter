411fe595a2115d1cb6476ca341bb472fcd088a0b
AshrafWzza <ashrafwzza2@gmail.com>
2022-08-03 15:49:38 +0200

    Date and Time

---
+++ lib/home/job_entries/entry_list_item.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter/home/job_entries/format.dart';
+import 'package:time_tracker_flutter/home/models/entry.dart';
+import 'package:time_tracker_flutter/home/models/job.dart';
+
+class EntryListItem extends StatelessWidget {
+  const EntryListItem({
+    required this.entry,
+    required this.job,
+    required this.onTap,
+  });
+
+  final Entry entry;
+  final Job job;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: _buildContents(context),
+            ),
+            Icon(Icons.chevron_right, color: Colors.grey),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final dayOfWeek = Format.dayOfWeek(entry.start);
+    final startDate = Format.date(entry.start);
+    final startTime = TimeOfDay.fromDateTime(entry.start).format(context);
+    final endTime = TimeOfDay.fromDateTime(entry.end).format(context);
+    final durationFormatted = Format.hours(entry.durationInHours);
+
+    final pay = job.ratePerHour * entry.durationInHours;
+    final payFormatted = Format.currency(pay);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(children: <Widget>[
+          Text(dayOfWeek, style: TextStyle(fontSize: 18.0, color: Colors.grey)),
+          SizedBox(width: 15.0),
+          Text(startDate, style: TextStyle(fontSize: 18.0)),
+          //Spread Operator ...
+          if (job.ratePerHour > 0.0) ...<Widget>[
+            Expanded(child: Container()),
+            Text(
+              payFormatted,
+              style: TextStyle(fontSize: 16.0, color: Colors.green[700]),
+            ),
+          ],
+        ]),
+        Row(children: <Widget>[
+          Text('$startTime - $endTime', style: TextStyle(fontSize: 16.0)),
+          Expanded(child: Container()),
+          Text(durationFormatted, style: TextStyle(fontSize: 16.0)),
+        ]),
+        if (entry.comment!.isNotEmpty)
+          Text(
+            entry.comment!,
+            style: TextStyle(fontSize: 12.0),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+          ),
+      ],
+    );
+  }
+}
+
+class DismissibleEntryListItem extends StatelessWidget {
+  const DismissibleEntryListItem({
+    required this.key,
+    required this.entry,
+    required this.job,
+    required this.onDismissed,
+    required this.onTap,
+  });
+
+  final Key key;
+  final Entry entry;
+  final Job job;
+  final VoidCallback onDismissed;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      background: Container(color: Colors.green),
+      secondaryBackground: Container(
+        alignment: Alignment.centerRight,
+        color: Colors.red,
+        child: const Icon(Icons.cancel),
+      ),
+      key: key,
+      //direction: DismissDirection.endToStart,
+      confirmDismiss: (DismissDirection direction) async {
+        return await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Delete Confirmation"),
+              content: const Text("Are you sure you want to delete this item?"),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: const Text("Delete")),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: const Text("Cancel"),
+                ),
+              ],
+            );
+          },
+        );
+      },
+      onDismissed: (direction) {
+        if (direction == DismissDirection.endToStart) {
+          onDismissed();
+          debugPrint('Deleted');
+        } else {
+          debugPrint('Added to favorite');
+        }
+      },
+      child: EntryListItem(
+        entry: entry,
+        job: job,
+        onTap: onTap,
+      ),
+    );
+  }
+}
